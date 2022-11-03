33f0cda80faf0dffab6285f92308cf7d4c18c2ce
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-03 16:05:36 +0100

    Add greetings model and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: 'Good morning')
+Greeting.create(greeting: 'Good afternoon')
+Greeting.create(greeting: 'Good evening')
+Greeting.create(greeting: 'Good night')
+Greeting.create(greeting: 'Good day')
